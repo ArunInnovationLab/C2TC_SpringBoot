e3dce8bdc11e18612c4de5ecbc168dac062063c0
Arunkumar <arunkumaryargoal@gmail.com>
2022-10-28 14:11:13 +0530

    commit

---
+++ ShoppingMallManagementSystem/src/main/java/com/tnsif/client/entities/Shop.java
@@ -0,0 +1,135 @@
+package com.tnsif.client.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+	
+@Entity
+public class Shop {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer shopId;
+	private String shopCategory;
+	
+	@OneToMany (mappedBy = "shop")
+	private List<Employee> shopEmployee;
+	private String shopName;
+	private String category;
+	private String shopStatus;
+	
+	@OneToOne
+	private ShopOwner shopOwner;
+	private String leaseStatus;
+	
+	@OneToMany(mappedBy = "shop")
+	private List<Item> items;
+	
+	public Shop() {
+		super();
+	}
+
+
+	public Shop(Integer shopId, String shopCategory, List<Employee> shopEmployee, String shopName, String category,
+			String shopStatus, ShopOwner shopOwner, String leaseStatus) {
+		super();
+		this.shopId = shopId;
+		this.shopCategory = shopCategory;
+		this.shopEmployee = shopEmployee;
+		this.shopName = shopName;
+		this.category = category;
+		this.shopStatus = shopStatus;
+		this.shopOwner = shopOwner;
+		this.leaseStatus = leaseStatus;
+	}
+
+
+	public Integer getShopId() {
+		return shopId;
+	}
+
+
+	public void setShopId(Integer shopId) {
+		this.shopId = shopId;
+	}
+
+
+	public String getShopCategory() {
+		return shopCategory;
+	}
+
+
+	public void setShopCategory(String shopCategory) {
+		this.shopCategory = shopCategory;
+	}
+
+
+	public List<Employee> getShopEmployee() {
+		return shopEmployee;
+	}
+
+
+	public void setShopEmployee(List<Employee> shopEmployee) {
+		this.shopEmployee = shopEmployee;
+	}
+
+
+	public String getShopName() {
+		return shopName;
+	}
+
+
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+	public String getShopStatus() {
+		return shopStatus;
+	}
+
+
+	public void setShopStatus(String shopStatus) {
+		this.shopStatus = shopStatus;
+	}
+
+
+	public ShopOwner getShopOwner() {
+		return shopOwner;
+	}
+
+
+	public void setShopOwner(ShopOwner shopOwner) {
+		this.shopOwner = shopOwner;
+	}
+
+
+	public String getLeaseStatus() {
+		return leaseStatus;
+	}
+
+
+	public void setLeaseStatus(String leaseStatus) {
+		this.leaseStatus = leaseStatus;
+	}
+
+	
+
+	
+	
+}
--- ShoppingMallManagementSystem/src/main/java/com/tnsif/client/entities/ShopOwner.java
+++ ShoppingMallManagementSystem/src/main/java/com/tnsif/client/entities/ShopOwner.java
@@ -6,79 +6,84 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
-public class Employee {
+public class ShopOwner {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private LocalDate dob;
-	private Float salary;
 	private String address;
-	private String designation;
-	private Integer shop_id;
+	private Integer shopId;
 	
-	@ManyToOne
+	@OneToOne
 	private Shop shop;
+
 	
-	public Employee() {
+	
+	public ShopOwner() {
 		super();
 	}
-	public Employee(Integer id, String name, LocalDate dob, Float salary, String address, String designation,
-			Integer shop_id) {
+
+	public ShopOwner(Integer id, String name, LocalDate dob, String address, Integer shopId, Shop shop) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.dob = dob;
-		this.salary = salary;
 		this.address = address;
-		this.designation = designation;
-		this.shop_id = shop_id;
+		this.shopId = shopId;
+		this.shop = shop;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public LocalDate getDob() {
 		return dob;
 	}
+
 	public void setDob(LocalDate dob) {
 		this.dob = dob;
 	}
-	public Float getSalary() {
-		return salary;
-	}
-	public void setSalary(Float salary) {
-		this.salary = salary;
-	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public String getDesignation() {
-		return designation;
+
+	public Integer getShopId() {
+		return shopId;
 	}
-	public void setDesignation(String designation) {
-		this.designation = designation;
+
+	public void setShopId(Integer shopId) {
+		this.shopId = shopId;
 	}
-	public Integer getShop_id() {
-		return shop_id;
+
+	public Shop getShop() {
+		return shop;
 	}
-	public void setShop_id(Integer shop_id) {
-		this.shop_id = shop_id;
+
+	public void setShop(Shop shop) {
+		this.shop = shop;
 	}
 	
 	
